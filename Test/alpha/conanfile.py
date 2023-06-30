af21cc400a355e96a8eef6f32fc00deec04176ff
Todiq <rodolphecolleie@gmail.com>
2023-06-30 17:29:00 +0530

    Multiple changes

--- Test/alpha/conanfile.py
+++ Test/alpha/conanfile.py
@@ -22,8 +22,8 @@ class Pkg(ConanFile):
 	}
 
 	def export_sources(self):
-		copy(self, "*.c*", src=self.recipe_folder, dst=self.export_sources_folder)
-		copy(self, "*.h*", src=self.recipe_folder, dst=self.export_sources_folder)
+		copy(self, "*.cpp", src=self.recipe_folder, dst=self.export_sources_folder)
+		copy(self, "*.hpp", src=self.recipe_folder, dst=self.export_sources_folder)
 		copy(self, "*CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder)
 
 	def requirements(self):
@@ -39,7 +39,7 @@ class Pkg(ConanFile):
 		self.options["zlib/*"].shared=True
 
 	def layout(self):
-		cmake_layout(self, generator="Ninja")
+		cmake_layout(self)
 		bt = "." if self.settings.os != "Windows" else str(self.settings.build_type)
 
 		self.cpp.source.components["headers"].includedirs = ["include"]
@@ -49,6 +49,8 @@ class Pkg(ConanFile):
 		self.cpp.build.components["alpha1_2"].libdirs = [os.path.join(bt, "alpha1/alpha1_2")]
 		self.cpp.source.components["alpha2"].includedirs = ["alpha2/include"]
 		self.cpp.build.components["alpha2"].libdirs = [os.path.join(bt, "alpha2")]
+		self.cpp.build.components["alpha2"].builddirs = [os.path.join(bt, "alpha2")]
+		self.cpp.package.components["alpha2"].resdirs = ["output"]
 
 	def generate(self):
 		tc = CMakeToolchain(self)
@@ -66,6 +68,7 @@ class Pkg(ConanFile):
 		cmake.install()
 
 	def package_info(self):
+		self.cpp_info.resdirs = ["share"]
 		self.cpp_info.components["headers"].set_property("cmake_target_name", "Alpha::headers")
 		self.cpp_info.components["alpha1_1"].libs = ["alpha1_1"]
 		self.cpp_info.components["alpha1_1"].set_property("cmake_target_name", "Alpha::alpha1_1")
--- Test/beta/conanfile.py
+++ Test/beta/conanfile.py
@@ -1,4 +1,3 @@
-from collections import namedtuple
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
 from conan.tools.files import copy, collect_libs
@@ -11,23 +10,59 @@ class Pkg(ConanFile):
 
 	# Binary configuration
 	settings = "os", "compiler", "build_type", "arch"
+	options = {
+		"shared": [True, False],
+		"fPIC": [True, False],
+	}
+	default_options = {
+		"shared": True,
+		"fPIC": False,
+	}
+
+	def export_sources(self):
+		copy(self, "*.cpp", src=self.recipe_folder, dst=self.export_sources_folder, excludes=["build"])
+		copy(self, "*.h", src=self.recipe_folder, dst=self.export_sources_folder, excludes=["build"])
+		copy(self, "*.hpp", src=self.recipe_folder, dst=self.export_sources_folder, excludes=["build"])
+		copy(self, "*CMakeLists.txt", src=self.recipe_folder, dst=self.export_sources_folder, excludes=["build"])
+		copy(self, "*.inl", src=self.recipe_folder, dst=self.export_sources_folder, excludes=["build"])
 
 	def requirements(self):
 		self.requires("alpha/1.0")
 
+	def config_options(self):
+		if self.settings.os == "Windows":
+			del self.options.fPIC
+
 	def configure(self):
+		if self.options.shared:
+			self.options.rm_safe("fPIC")
 		self.options["alpha/*"].shared=True
 
 	def layout(self):
 		cmake_layout(self, generator="Ninja")
+		bt = "." if self.settings.os != "Windows" else str(self.settings.build_type)
+
+		self.cpp.source.components["test"].includedirs = ["include"]
+		self.cpp.build.components["test"].libdirs = [bt]
 
 	def generate(self):
-		tc = CMakeToolchain(self)
-		tc.generate()
-		d = CMakeDeps(self)
-		d.generate()
+		ct = CMakeToolchain(self, generator="Ninja")
+		ct.generate()
+		cd = CMakeDeps(self)
+		cd.generate()
 
 	def build(self):
 		cmake = CMake(self)
 		cmake.configure()
 		cmake.build()
+
+	def package(self):
+		cmake = CMake(self)
+		cmake.install()
+
+	def package_info(self):
+		self.cpp_info.components["test"].libs = [f"test"]
+		self.cpp_info.components["test"].set_property("cmake_target_name", f"Beta::test")
+		self.cpp_info.components["test"].requires = [
+			"alpha::alpha"
+		]
