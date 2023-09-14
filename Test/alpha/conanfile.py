22e5f9ec248bbe673a21d08e237c404cc6d16405
RCO <r.colleie@castsoftware.com>
2023-09-14 17:08:40 +0530

    Add windows profile

--- Test/alpha/conanfile.py
+++ Test/alpha/conanfile.py
@@ -31,7 +31,7 @@ class Pkg(ConanFile):
 
 	def config_options(self):
 		if self.settings.os == "Windows":
-			del self.options.fPIC
+			self.options.rm_safe("fPIC")
 
 	def configure(self):
 		if self.options.shared:
